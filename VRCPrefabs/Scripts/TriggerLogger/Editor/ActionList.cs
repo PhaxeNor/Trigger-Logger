fbd888016c30fce917b55c76cad3977606fb0822
PhaxeNor <phaxe@phaxe.com>
2018-08-02 10:36:18 +0200

    Create action list for vrc trigger actions

---
+++ VRCPrefabs/Scripts/TriggerLogger/Editor/ActionList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using VRCSDK2;
+
+/**
+ * Author: PhaxeNor
+ * Github: https://github.com/PhaxeNor/Trigger-Logger
+ */
+namespace VRCP.TriggerLogger
+{
+	public class ActionItem
+	{
+		public VRC_EventHandler.VrcEventType type { get; set; }
+		public int total { get; set; }
+
+		public string getTypeName()
+		{
+			return type.ToString ();
+		}
+	}
+
+	public class ActionList
+	{
+		public List<ActionItem> list { get; set; }
+
+		public void initList()
+		{
+			list = new List<ActionItem> ();
+
+			foreach (VRC_EventHandler.VrcEventType e in Enum.GetValues(typeof(VRC_EventHandler.VrcEventType))) {
+				list.Add (new ActionItem() { type = e, total = 0 });
+			}
+		}
+	}
+}
